378ff45632498de2e327b040742cb4d27be6f42a
Louise Ly <ly.louise07@gmail.com>
2023-07-26 15:00:03 -0500

    Register loading component with router event

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
 import Head from 'next/head';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import { Provider } from 'react-redux';
+import Loading from '@/components/reusables/Loading';
 import Layout from '@/components/layout/Layout';
 import { store } from '@/store';
 
@@ -10,6 +13,24 @@ export default function App({
   Component,
   pageProps: { session, ...pageProps }
 }: AppProps) {
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleStart = () => setIsLoading(true);
+    const handleComplete = () => setIsLoading(false);
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleComplete);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleComplete);
+      router.events.off('routeChangeError', handleComplete);
+    };
+  }, []);
+
   return (
     <>
       <Head>
@@ -20,9 +41,13 @@ export default function App({
       </Head>
       <SessionProvider session={session}>
         <Provider store={store}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
+          {isLoading ? (
+            <Loading />
+          ) : (
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          )}
         </Provider>
       </SessionProvider>
     </>
